4d2e254b0698637b94349a2b6f946e090981396d
msyon2 <suyon.korea@gmail.com>
2022-04-28 01:17:38 +0900

    css cleanup & js adjustment

--- js/script.js
+++ js/script.js
@@ -1,26 +1,13 @@
 let langWrapper = $(".lang_box"),
-langBox = langWrapper.find(".lang"),
-langOption = langWrapper.find("li.lang_option");
+langBox = langWrapper.find(".lang");
 
 //language option box toggle open
 langBox.on({
 	click: function (e) {
 		e.preventDefault();
-		langBox.toggleClass("open")
+		langBox.toggleClass("open");
 	}
 })
-//language option select
-langOptionToggle = $.each(langOption, function(i,e){
-	current = $(this);
-	current.click(function (e) {
-		e.preventDefault();
-
-		langOption.removeClass("active");
-		langOption.eq(i).addClass("active");
-		langBox.removeClass("open");
-	})
-})
-langOption.eq(0).addClass("active");
 
 
 
